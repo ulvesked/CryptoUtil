a2b6dc7a6fae94eae2db0fb6d24f52d9eb48f474
Anders Knutsson <anders@Anderss-MacBook-Pro.local>
2018-09-27 09:02:55 +0200

    RSAUtil

---
+++ Example/Pods/Target Support Files/CryptoUtil/CryptoUtil-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CryptoUtil : NSObject
+@end
+@implementation PodsDummy_CryptoUtil
+@end
--- RSAUtil.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RSAUtil.swift
-//  CryptoUtil
-//
-//  Created by Anders Knutsson on 2018-09-26.
-//
-
-import Foundation
